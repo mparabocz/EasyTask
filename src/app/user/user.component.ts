0e181ef3cc7e1292d14691e6143af259873ab513
Marcelo Parabocz <macerno@gmail.com>
2026-03-30 17:08:12 -0300

    feat: taks and user model

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,6 +1,6 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 
-type User = { id: string; name: string; avatar: string };
+import { type User } from './user.model';
 
 @Component({
   selector: 'app-user',
